2ee53c721658fded3adf451a8d37c11e7b9de6c4
Lia Odette Pon <ponlia@mail.utoronto.ca>
2018-06-23 14:59:33 -0400

    commented out DefinitionHandler and getSpeechDefinition

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -73,57 +73,57 @@ const QuizHandler = {
   },
 };
 
-const DefinitionHandler = {
-  canHandle(handlerInput) {
-    console.log("Inside DefinitionHandler");
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const request = handlerInput.requestEnvelope.request;
+// const DefinitionHandler = {
+//   canHandle(handlerInput) {
+//     console.log("Inside DefinitionHandler");
+//     const attributes = handlerInput.attributesManager.getSessionAttributes();
+//     const request = handlerInput.requestEnvelope.request;
 
-    return attributes.state !== states.QUIZ &&
-           request.type === 'IntentRequest' &&
-           request.intent.name === 'AnswerIntent';
-  },
-  handle(handlerInput) {
-    console.log("Inside DefinitionHandler - handle");
-    //GRABBING ALL SLOT VALUES AND RETURNING THE MATCHING DATA OBJECT.
-    const item = getItem(handlerInput.requestEnvelope.request.intent.slots);
-    const response = handlerInput.responseBuilder;
+//     return attributes.state !== states.QUIZ &&
+//            request.type === 'IntentRequest' &&
+//            request.intent.name === 'AnswerIntent';
+//   },
+//   handle(handlerInput) {
+//     console.log("Inside DefinitionHandler - handle");
+//     //GRABBING ALL SLOT VALUES AND RETURNING THE MATCHING DATA OBJECT.
+//     const item = getItem(handlerInput.requestEnvelope.request.intent.slots);
+//     const response = handlerInput.responseBuilder;
 
-    //IF THE DATA WAS FOUND
-    if (item && item[Object.getOwnPropertyNames(data[0])[0]] !== undefined) {
-      if (useCardsFlag) {
-        response.withStandardCard(
-          getCardTitle(item),
-          getTextDescription(item),
-          getSmallImage(item),
-          getLargeImage(item))
-      }
+//     //IF THE DATA WAS FOUND
+//     if (item && item[Object.getOwnPropertyNames(data[0])[0]] !== undefined) {
+//       if (useCardsFlag) {
+//         response.withStandardCard(
+//           getCardTitle(item),
+//           getTextDescription(item),
+//           getSmallImage(item),
+//           getLargeImage(item))
+//       }
 
-      if(supportsDisplay(handlerInput)) {
-        const image = new Alexa.ImageHelper().addImageInstance(getLargeImage(item)).getImage();
-        const title = getCardTitle(item);
-        const primaryText = new Alexa.RichTextContentHelper().withPrimaryText(getTextDescription(item, "<br/>")).getTextContent();
-        response.addRenderTemplateDirective({
-          type: 'BodyTemplate2',
-          backButton: 'visible',
-          image,
-          title,
-          textContent: primaryText,
-        });
-      }
-      return response.speak(getSpeechDescription(item))
-              .reprompt(repromptSpeech)
-              .getResponse();
-    }
-    //IF THE DATA WAS NOT FOUND
-    else
-    {
-      return response.speak(getBadAnswer(item))
-              .reprompt(getBadAnswer(item))
-              .getResponse();
-    }
-  }
-};
+//       if(supportsDisplay(handlerInput)) {
+//         const image = new Alexa.ImageHelper().addImageInstance(getLargeImage(item)).getImage();
+//         const title = getCardTitle(item);
+//         const primaryText = new Alexa.RichTextContentHelper().withPrimaryText(getTextDescription(item, "<br/>")).getTextContent();
+//         response.addRenderTemplateDirective({
+//           type: 'BodyTemplate2',
+//           backButton: 'visible',
+//           image,
+//           title,
+//           textContent: primaryText,
+//         });
+//       }
+//       return response.speak(getSpeechDescription(item))
+//               .reprompt(repromptSpeech)
+//               .getResponse();
+//     }
+//     //IF THE DATA WAS NOT FOUND
+//     else
+//     {
+//       return response.speak(getBadAnswer(item))
+//               .reprompt(getBadAnswer(item))
+//               .getResponse();
+//     }
+//   }
+// };
 
 const QuizAnswerHandler = {
   canHandle(handlerInput) {
@@ -387,9 +387,9 @@ function getBackgroundImage(label, height = 1024, width = 600) {
     .replace("{2}", label);
 }
 
-function getSpeechDescription(item) {
-  return `${item.StateName} is the ${item.StatehoodOrder}th state, admitted to the Union in ${item.StatehoodYear}.  The capital of ${item.StateName} is ${item.Capital}, and the abbreviation for ${item.StateName} is <break strength='strong'/><say-as interpret-as='spell-out'>${item.Abbreviation}</say-as>.  I've added ${item.StateName} to your Alexa app.  Which other state or capital would you like to know about?`;
-}
+// function getSpeechDescription(item) {
+//   return `${item.StateName} is the ${item.StatehoodOrder}th state, admitted to the Union in ${item.StatehoodYear}.  The capital of ${item.StateName} is ${item.Capital}, and the abbreviation for ${item.StateName} is <break strength='strong'/><say-as interpret-as='spell-out'>${item.Abbreviation}</say-as>.  I've added ${item.StateName} to your Alexa app.  Which other state or capital would you like to know about?`;
+// }
 
 function formatCasing(key) {
   return key.split(/(?=[A-Z])/).join(' ');
