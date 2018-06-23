df5d8e84fe96c3c53c3c6e921926b55b73e19470
Isabelle Wei <isabellewei@localhost.localdomain>
2018-06-23 12:27:35 -0400

    modify messages

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -355,11 +355,11 @@ const states = {
   QUIZ: `_QUIZ`,
 };
 
-const welcomeMessage = `Welcome to the United States Quiz Game!  You can ask me about any of the fifty states and their capitals, or you can ask me to start a quiz.  What would you like to do?`;
-const startQuizMessage = `OK.  I will ask you 10 questions about the United States. `;
-const exitSkillMessage = `Thank you for playing the United States Quiz Game!  Let's play again soon!`;
-const repromptSpeech = `Which other state or capital would you like to know about?`;
-const helpMessage = `I know lots of things about the United States.  You can ask me about a state or a capital, and I'll tell you what I know.  You can also test your knowledge by asking me to start a quiz.  What would you like to do?`;
+const welcomeMessage = `Welcome to Study Buddy! Let's have some fun! Do you want to start?  `;
+const startQuizMessage = `Alright, let's start!`;
+const exitSkillMessage = `Thanks for playing with me today! See you soon!`;
+const repromptSpeech = `Say something`;
+const helpMessage = `I love the alphabet and I know lots of things about animals. Let's play some games!`;
 const useCardsFlag = true;
 
 /* HELPER FUNCTIONS */
